6750d291a69544848ace09d8e85c6e4e54d48b94
kontsio1 <kontsio18@gmail.com>
2024-12-11 23:18:18 +0000

    gigs migration

--- Models/Gigs/UserGig.cs
+++ Models/Gigs/UserGig.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using GigApp.Models.Users;
 
 namespace GigApp.Models;
 
@@ -8,8 +9,12 @@ public class UserGig
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
+
     [ForeignKey("User")]
-    public string UserId { get; set; } = null!;
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
+
     [ForeignKey("Gig")]
-    public string GigId { get; set; } = null!;
+    public Guid GigId { get; set; }
+    public virtual Gig Gig { get; set; }
 }
